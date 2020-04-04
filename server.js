283988a4b5f40f95cc50cd6265f761a47e9e54cf
raquelacevedo <raquel.e.acevedo@gmail.com>
2020-04-04 12:01:33 -0400

    heroku db config

--- server.js
+++ server.js
@@ -15,10 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://user:password1@ds047772.mlab.com:47772/heroku_dbnpvq73",
+  {
+    useMongoClient: true
+  }
+)
 
 // routes
 app.use(require("./routes/api.js"));
